a79a70a45b5ae45aa1f1942031298c244c664656
Nikunj <mnikunj262@gmail.com>
2025-12-22 10:29:34 +0530

    update: migration

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,4 +1,5 @@
-export { user } from "@/db/schema/tables/user"
+export { user, players } from "@/db/schema/tables/user"
 export { tags, status, eventsTags } from "@/db/schema/tables/tags"
 export { events } from "@/db/schema/tables/event"
-export { userRelation, eventsRelation, tagsRelation, statusRelation, eventsTagRelation } from "@/db/schema/relations"
+export { teams } from "@/db/schema/tables/teams"
+export { userRelation, eventsRelation, tagsRelation, statusRelation, teamsRelation, playersRelation, eventsTagRelation } from "@/db/schema/relations"
--- db/schema/relations/index.ts
+++ db/schema/relations/index.ts
@@ -1,10 +1,12 @@
 import { relations } from "drizzle-orm";
-import { user } from "../tables/user";
+import { players, user } from "../tables/user";
 import { events } from "../tables/event";
 import { eventsTags, status, tags } from "../tables/tags";
+import { teams } from "../tables/teams";
 
 export const userRelation = relations(user, ({ many }) => ({
-  events: many(events)
+  events: many(events),
+  players: many(players),
 }))
 
 export const eventsRelation = relations(events, ({ many, one }) => ({
@@ -26,6 +28,21 @@ export const tagsRelation = relations(tags, ({ many }) => ({
 export const statusRelation = relations(status, ({ many }) => ({
   events: many(events)
 }))
+
+export const teamsRelation = relations(teams, ({ many }) => ({
+  players: many(players),
+}));
+
+export const playersRelation = relations(players, ({ one }) => ({
+  user: one(user, {
+    fields: [players.userId],
+    references: [user.id],
+  }),
+  team: one(teams, {
+    fields: [players.teamId],
+    references: [teams.id],
+  }),
+}));
 
 export const eventsTagRelation = relations(eventsTags, ({ one }) => ({
   event: one(events, {
