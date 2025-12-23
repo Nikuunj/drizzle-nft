5de422d998cbd95ea6ce9f5ae2bdde37eb8aed99
Nikunj <mnikunj262@gmail.com>
2025-12-23 17:38:17 +0530

    get: specific events details

--- app/api/events/[id]/route.ts
+++ app/api/events/[id]/route.ts
@@ -1,9 +1,45 @@
+import { db } from "@/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(req: NextRequest) {
+export async function GET(_req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
+  const eventDetails = await db.query.events.findFirst({
+    where: (events, { eq }) => eq(events.id, id),
+    with: {
+      user: true,
+      status: {
+        columns: {
+          title: true
+        }
+      },
+      eventsTags: {
+        with: {
+          tag: {
+            columns: {
+              title: true
+            }
+          }
+        }
+      }
+    },
+  });
+
+  if (!eventDetails) {
+    return NextResponse.json({
+      message: "event is not found"
+    }, { status: 404 })
+  }
+  if (eventData.data.mode !== "create") {
+    return NextResponse.json(
+      { message: "Only create mode is allowed for this endpoint" },
+      { status: 400 }
+    );
+  }
   return NextResponse.json({
-    msg: 'hi from evernt/id/router get'
-  })
+    eventDetails,
+    message: `hi from event/id/router get ${id}`,
+  });
 }
 
 export function PUT(req: NextRequest) {
