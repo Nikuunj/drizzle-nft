a9c9ccf5eb10e3387a6a0e49bcf3317844613f67
Nikunj <mnikunj262@gmail.com>
2025-12-23 15:07:31 +0530

    update: create and get all events

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,13 +1,55 @@
+import { db } from "@/db";
+import { status } from "@/db/schema";
+import { events, eventsSchema } from "@/db/schema/tables/event";
+import { desc, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(req: NextRequest) {
+export async function GET(req: NextRequest) {
+  const page = Number(req.nextUrl.searchParams.get("page") ?? 1)
+  const limit = 10;
+  const offset = (page - 1) * limit;
+  const eventsList = await db
+    .select({
+      id: events.id,
+      userId: events.userId,
+      title: events.title,
+      organizationName: events.organizationName,
+      description: events.description,
+      location: events.location,
+      price: events.price,
+      dateTime: events.dateTime,
+      createAt: events.createAt,
+      status: status.title,
+    })
+    .from(events)
+    .innerJoin(status, eq(events.statusId, status.id))
+    .orderBy(desc(events.dateTime))
+    .limit(limit)
+    .offset(offset);
   return NextResponse.json({
-    msg: `hi rom event/router get`
+    eventsList,
+    page,
+    limit
   })
 }
 
-export function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const eventData = eventsSchema.safeParse(body);
+
+  if (!eventData.success) {
+    return NextResponse.json(
+      {
+        error: eventData.error.format(),
+        message: "Invalid data formate"
+      },
+      { status: 400 }
+    )
+  }
+
+  await db.insert(events).values(eventData.data);
+
   return NextResponse.json({
-    msg: `hi rom event/router post `
+    msg: `hi rom event/router event created`
   })
 }
