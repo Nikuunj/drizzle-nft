2795dd4ce55e0bc56d7fb966596a802e2288979a
Nikunj <mnikunj262@gmail.com>
2025-12-23 17:37:36 +0530

    update: create end point with tags

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,8 +1,9 @@
 import { db } from "@/db";
 import { status } from "@/db/schema";
-import { events, eventsSchema } from "@/db/schema/tables/event";
+import { events, eventsSchema, eventsTags } from "@/db/schema/tables/event";
 import { desc, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
+import { crypto } from "next/dist/compiled/@edge-runtime/primitives";
 
 export async function GET(req: NextRequest) {
   const page = Number(req.nextUrl.searchParams.get("page") ?? 1)
@@ -46,10 +47,40 @@ export async function POST(req: NextRequest) {
       { status: 400 }
     )
   }
+  try {
+    const newEventId = crypto.randomUUID();
+    const { tagIds, mode, ...rest } = eventData.data;
 
-  await db.insert(events).values(eventData.data);
+    if (mode !== "create") {
+      return NextResponse.json({ message: "Only creation allowed on this route" }, { status: 405 });
+    }
 
-  return NextResponse.json({
-    msg: `hi rom event/router event created`
-  })
+    await db.transaction(async (tx) => {
+      await tx.insert(events).values({
+        ...rest,
+        id: newEventId,
+        dateTime: new Date(rest.dateTime),
+      });
+
+      if (tagIds && tagIds.length > 0) {
+        const tagEntries = tagIds.map((tagId) => ({
+          eventsId: newEventId,
+          tagId: tagId,
+        }));
+
+        await tx.insert(eventsTags).values(tagEntries);
+      }
+    });
+
+    return NextResponse.json({
+      message: "Event and tags created successfully",
+      id: newEventId,
+    });
+  } catch (error) {
+    console.error("Database Error:", error);
+    return NextResponse.json(
+      { message: "Failed to create event" },
+      { status: 500 }
+    );
+  }
 }
