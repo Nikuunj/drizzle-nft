1ed4f9d0a6189e18c1773d1d076d4c857cfd3221
Nikunj <mnikunj262@gmail.com>
2025-12-24 16:26:44 +0530

    details page

--- app/d/[eventId]/page.tsx
+++ app/d/[eventId]/page.tsx
@@ -1,12 +1,23 @@
-import { NEXT_PUBLIC_BASE_URL } from "@/config";
+import Details from "@/components/details/Details"
+import EventSummary from "@/components/details/EventSummary";
+import Tags from "@/components/details/Tags";
+import Teams from "@/components/details/Teams";
 
 async function EventDetailsPage({ params }: { params: Promise<{ eventId: string }> }) {
 
   const { eventId } = await params;
   return (
-    <div>hif from event details page eventId is ------  {eventId}
-      <br />
-      {NEXT_PUBLIC_BASE_URL}
+    <div className="px-[30px] py-4 gap-5 flex flex-col">
+      <div className="gap-5 flex flex-col">
+        <div className="gap-5 flex">
+          <Details />
+          <EventSummary />
+        </div>
+        <div className="gap-3 flex ">
+          <Teams />
+          <Tags />
+        </div>
+      </div>
     </div>
   )
 }
